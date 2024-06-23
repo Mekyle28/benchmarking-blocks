b452c5f35029a6a6bd1512bc6747fab21264b0e8
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-22 20:46:23 -0400

    added benchmark function

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,11 +1,18 @@
+
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield;
+  end_time = Time.now
+  running_time = end_time - start_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
 long_string = "apple"*100000000
  
-running_time = benchmark { long_string.reverse }
- 
+running_time = benchmark { long_string.reverse } 
 puts "string.reverse took #{running_time} seconds to run"
+
+
+running_time = benchmark { (1..100000000).reduce(:+) }
+puts "Summing numbers from 1 to 100000000 took #{running_time} seconds"
